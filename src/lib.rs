64b86cdaba521ff671fa57962c906b7be613deba
lake <102535481@qq.com>
2024-01-11 17:52:37 +0800

    增加群

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 //! # aliyun-oss-rust-sdk
 //! [![Latest Version](https://img.shields.io/crates/v/aliyun-oss-rust-sdk.svg)](https://crates.io/crates/aliyun-oss-rust-sdk)
 //!
-//! 阿里云 © Alibaba Cloud Official Oss SDK(标准库)
+//! 阿里云 © Alibaba Cloud Official Oss SDK (标准库)
+//!
+//! [![QQ群](https://img.shields.io/badge/QQ%E7%BE%A4-799168925-blue)](http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=dLoye8pBcO60zGzqLjGO0l-GgMIaf6wQ&authKey=LfxBdZ5A%2F9eWJbKpzTcuWPjmQu5UdIJ3TVTpqRAQYkCID50WLkYoIXcGxGKzupG3&noverify=0&group_code=799168925)
 //!
 //! # 使用指南
 //!
